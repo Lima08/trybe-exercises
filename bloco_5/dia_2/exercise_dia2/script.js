cfa7ec8da826d9acfb1cd0fcde8ccc81b987ba3a
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-04-09 12:16:54 -0300

    Adicionado class Description nas h3s.

--- bloco_5/dia_2/exercise_dia2/script.js
+++ bloco_5/dia_2/exercise_dia2/script.js
@@ -52,3 +52,19 @@ for (index = 0; index < 3; index +=1) {
   div.appendChild(tagh3);
 }
 
+// EX parte 2
+
+
+// 3- Remova o div criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+// 4- Centralize o div criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto do div ;
+// 5- Troque a cor de fundo do elemento pai da div criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+// 6- Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+
+// 1- Adicione a classe title na tag h1 criada;
+h1.className += 'title';
+
+// 2- Adicione a classe description nas 3 tags h3 criadas;
+let h3s = document.querySelectorAll('h3');
+for (h3 of h3s) {
+  h3.className += 'description';
+}
