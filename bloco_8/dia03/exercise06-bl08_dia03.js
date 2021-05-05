6d446e8f18c4fcadf8ae80e195e56ba4ea5da8a7
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-05-04 23:46:53 -0300

    Finalizado ex6 dia 3 bloco 8.

--- bloco_8/dia03/exercise06-bl08_dia03.js
+++ bloco_8/dia03/exercise06-bl08_dia03.js
@@ -1,64 +1,64 @@
 // Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
 
-const assert = require('assert');
+const assert = require("assert");
 
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
+      name: "George R. R. Martin",
       birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
+      name: "Stephen King",
       birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
@@ -67,14 +67,21 @@ const books = [
 
 // Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
 function oldBooks() {
-  
+  let oldBook = [];
+
+  const book = books.filter((book) => book.releaseYear < 1961);
+
+  book.forEach((bookOld) => {
+    oldBook.push(bookOld.name);
+  });
+  return oldBook;
 }
 
 const expectedResult = [
-  'O Senhor dos Anéis',
-  'Fundação',
-  'O Chamado de Cthulhu',
+  "O Senhor dos Anéis",
+  "Fundação",
+  "O Chamado de Cthulhu",
 ];
 
-console.log(oldBooks);
-// assert.deepStrictEqual(oldBooks(), expectedResult);
+// console.log(oldBooks());
+assert.deepStrictEqual(oldBooks(), expectedResult);
