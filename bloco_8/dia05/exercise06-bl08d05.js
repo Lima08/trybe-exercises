791ef67a0a4159af079d5bb59cf99dfd825a9182
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-05-06 16:06:06 -0300

    Feito ex6 dia 5 bl8

--- bloco_8/dia05/exercise06-bl08d05.js
+++ bloco_8/dia05/exercise06-bl08d05.js
@@ -7,7 +7,11 @@ const palio = ['Palio', 'Fiat', 2019];
 const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
-// escreva toObject abaixo
+toObject = ([name, brand, year]) => { return {name, brand, year}};
+
+console.log(toObject(palio));
+console.log(toObject(shelbyCobra));
+console.log(toObject(chiron));
 
 assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
 assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
