5bc95f037cceda2bf1fa4a0b978965940de3c68b
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-05-25 15:37:11 -0300

    Atualizando repo

--- bloco_10/dia02/src/ex01-uppercase.js
+++
@@ -1,5 +0,0 @@
-const uppercase = (str, callBack) => {
-  callBack(str.toUpperCase());
-};
-
-module.exports = uppercase
--- bloco_10/dia02/test/ex01-upperCase.spec.js
+++
@@ -1,9 +0,0 @@
-const upperCase = require('../src/ex01-uppercase');
-
-it('Testing if function uppercase returns LIMA', () => {
-  expect.assertions(1);
-
-  upperCase('lima', (result)=> {
-    expect(result).toEqual('LIMA')
-  })
-});
