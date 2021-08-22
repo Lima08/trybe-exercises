ed39dfd05e1504a77af423f766b176f16da3b59a
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-08-22 17:32:17 -0300

    Feito exercicios do dia.

--- Back-end/Bloco20/PArte03/exercise03.sql
+++ Back-end/Bloco20/PArte03/exercise03.sql
@@ -66,24 +66,43 @@ INSERT INTO Vendas(fornecimento, quantity, order_date)
     (9, 5, '2020-05-13 14:05:19');
 
 -- Escreva uma query para exibir todas as peças que começam com GR .
-
+Use PecasFornecedores;
+SELECT * FROM Pecas
+WHERE name LIKE 'GR%';
 
 -- Escreva uma query para exibir todos os fornecimentos que contenham a peça com code 2 . Organize o resultado por ordem alfabética de fornecedor.
+SELECT * FROM Fornecimentos;
+SELECT * FROM Fornecimentos
+WHERE peca = 2
+ORDER BY Fornecedor;
 
 
 -- Escreva uma query para exibir as peças, preço e fornecedor de todos os fornecimentos em que o código do fornecedor tenha a letra N .
+SELECT peca, Preco, Fornecedor FROM Fornecimentos
+WHERE Fornecedor LIKE '%N%';
 
 
 -- Escreva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA). Ordene os resultados em ordem alfabética decrescente.
+SELECT * FROM Fornecedores;
+SELECT * FROM Fornecedores
+WHERE name LIKE '%LTDA'
+ORDER BY name DESC;
 
 
 -- Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código.
+SELECT * FROM Fornecedores;
+SELECT COUNT(*) AS `Empresas com F no nome` FROM Fornecedores
+WHERE code LIKE '%F%';
 
 
 -- Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00 . Ordene os resultados por ordem crescente.
-
+SELECT * FROM Fornecimentos;
+SELECT * FROM Fornecimentos
+WHERE Preco BETWEEN 16 AND 39
+ORDER BY Preco;
 
 -- Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019 .
-
-
+SELECT * FROM Vendas;
+SELECT COUNT(*) FROM Vendas
+WHERE DATE(order_date) BETWEEN '2018/04/15' AND '2019/07/30';
 
