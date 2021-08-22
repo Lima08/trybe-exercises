730b0b1952db4ccefcee968c978cc553f3f2641b
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-08-22 11:48:31 -0300

    Finalizado exercicios bonus pt2.

--- Back-end/Bloco20/Parte02/bonus-Parte2.sql
+++ Back-end/Bloco20/Parte02/bonus-Parte2.sql
@@ -47,20 +47,37 @@ INSERT INTO Provides(Piece, Provider, Price)
     (4, 'RBT', 7);
 
 -- Esse banco de dados é de uso livre, sendo licenciado de acordo com os termos deste link: https://creativecommons.org/licenses/by-sa/3.0/
+-- Queries das tabelas que usaremos
+USE PiecesProviders;
+SELECT * FROM Provides;
+SELECT * FROM Pieces;
+SELECT * FROM Providers;
+
 -- Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT .
+SELECT Piece, Price FROM Provides
+WHERE Provider = 'RBT';
 
 
 -- Escreve uma query para exibir todas as informações das cinco peças com os maiores preços.
-
+SELECT Piece, Price FROM Provides
+ORDER BY Price DESC
+LIMIT 5;
 
 -- Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.
-
+SELECT DISTINCT Provider, Price FROM Provides
+ORDER BY Price DESC
+LIMIT 4
+OFFSET 2;
 
 -- Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL . Ordene o resultado pelos preços das peças de forma decrescente.
-
+SELECT * FROM Provides
+WHERE Provider = 'HAL'
+ORDER BY Price DESC;
 
 -- Escreva uma query para exibir por quantas empresas a peça 1 é provida.
-
+SELECT COUNT(DISTINCT Provider)
+FROM Provides
+WHERE Piece = 1;
 
 -- Exercício 7 : Usando WHERE , faça os exercícios deste link: https://www.w3schools.com/sql/exercise.asp?filename=exercise_where1
 
