07a44e40a4ad9ab351144fa47fa94268212220fd
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-08-22 13:59:03 -0300

    Feito exercicios de fixaçao do comando LIKE.

--- Back-end/Bloco20/PArte03/exercise03.sql
+++ Back-end/Bloco20/PArte03/exercise03.sql
@@ -34,27 +34,41 @@ WHERE (amount = 0.99 OR amount = 2.99) AND staff_id = 2;
 
 -- Entre no banco de dados sakila e siga as instruções (e guarde as queries para conferir posteriormente):
 -- Precisamos identificar os dados do cliente com o e-mail LEONARD.SCHOFIELD@sakilacustomer.org .
-
+use sakila;
+SELECT * from customer
+WHERE email = 'LEONARD.SCHOFIELD@sakilacustomer.org';
 
 -- Precisamos de um relatório dos nomes dos clientes, em ordem alfabética , que não estão mais ativos no nosso sistema e pertencem à loja com o id = 2 , e não inclua o cliente KENNETH no resultado.
-
+SELECT first_name, last_name FROM customer
+WHERE active = 0
+AND store_id = 2
+AND first_name <> 'KENNETH'
+ORDER BY first_name ASC, last_name ASC;
 
 
 -- O setor financeiro quer saber título, descrição, ano de lançamento e valor do custo de substituição ( replacement_cost ), dos 100 filmes com o maior custo de substituição, do valor mais alto ao mais baixo, entre os filmes feitos para menores de idade e que têm o custo mínimo de substituição de $18,00 dólares. Em caso de empate, ordene em ordem alfabética pelo título.
+SELECT * FROM film;
+SELECT title, `description`, release_year, replacement_cost, rating FROM film
+WHERE rating = 'PG'
+AND replacement_cost >= 18.00
+ORDER BY replacement_cost DESC, title ASC
+LIMIT 100;
 
 
 -- Quantos clientes estão ativos e na loja 1 ?
-
+SELECT COUNT(*) AS `Quantidade de clientes ativos` FROM customer
+WHERE active = 1
+AND store_id = 1;
 
 -- Mostre todos os detalhes dos clientes que não estão ativos na loja 1 .
-
+SELECT * FROM customer
+WHERE active = 0 AND store_id = 1;
 
 -- Precisamos descobrir quais são os 50 filmes feitos apenas para adultos com a menor taxa de aluguel, para que possamos fazer uma divulgação melhor desses filmes. Em caso de empate, ordene em ordem alfabética pelo título.
-
-
--- Você vai se deparar também com casos em que você só tem parte de uma informação, ou em que precisa criar queries mais dinâmicas. Para esses casos, temos o LIKE . Como assim? É o que veremos a seguir.
-
-
+SELECT * FROM film
+WHERE rating = 'NC-17'
+ORDER BY rental_duration, title
+LIMIT 50;
 
 ------------------------------------------------------------------------
 -------Como criar pesquisas mais dinâmicas e maleáveis usando o LIKE-------
@@ -97,24 +111,34 @@ WHERE title LIKE 'E__%';
 ----------- Para Fixar -----------
 -- Para consolidar esse conhecimento, brinque com os resultados que serão encontrados usando os comandos acima e tente criar suas próprias condições. Depois de ter praticado um pouco com eles, tente encontrar as seguintes informações:
 -- Mostre todos os detalhes dos filmes que contêm a palavra ace no nome.
+SELECT * FROM film
+WHERE title LIKE '%ace%';
 
 
 -- Mostre todos os detalhes dos filmes cujas descrições finalizam com china .
-
+SELECT * FROM film
+WHERE `description` LIKE '%china';
 
 -- Mostre todos os detalhes dos dois filmes cujas descrições contêm a palavra girl e o título finaliza com a palavra lord .
-
+SELECT * FROM film
+WHERE `description` LIKE '%girl%'
+AND title LIKE '%lord';
 
 -- Mostre os dois casos em que, a partir do 4° caractere no título do filme, tem-se a palavra gon .
-
+SELECT * FROM film
+WHERE title LIKE '___gon%';
 
 -- Mostre o único caso em que, a partir do 4° caractere no título do filme, tem-se a palavra gon e a descrição contém a palavra Documentary .
 
+SELECT * FROM film
+WHERE title LIKE '___gon%' AND `description` LIKE '%Documentary%';
 
 -- Mostre os dois filmes cujos títulos ou finalizam com academy ou iniciam com mosquito .
-
+SELECT * FROM film
+WHERE title LIKE '%academy' OR title LIKE 'mosquito%';
 
 -- Mostre os seis filmes que contêm as palavras monkey e sumo em suas descrições.
-
+SELECT * FROM film
+WHERE `description` LIKE '%monkey%' AND `description` LIKE '%sumo%';
 
 ----------------------------------------------------------------
