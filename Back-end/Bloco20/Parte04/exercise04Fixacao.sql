fd556ee2fa6fabfc784a051474349067f2d35bd2
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-08-22 21:43:11 -0300

    Salvo exercicios de fixaçao apra fazer.

--- Back-end/Bloco20/Parte04/exercise04Fixacao.sql
+++ Back-end/Bloco20/Parte04/exercise04Fixacao.sql
@@ -101,37 +101,78 @@ VALUES
 (6, 5);
 
 
-
 --------------------------------------------------------
------- O UPDATE te permite alterar valores de uma tabela com base em alguma condição. ---
+------ O UPDATE te permite alterar valores de uma tabela com base em alguma condição. --------------
+UPDATE sakila.staff
+SET first_name = 'Ravein'
+WHERE first_name = 'Rannveig';
+
+-- Uma curiosidade sobre o UPDATE e o DELETE no MySQL Server é que, por padrão, existe uma configuração chamada safe updates mode que só vai te permitir executá-los caso eles incluam quais IDs devem ser modificados. Então, caso você tente fazer a query abaixo, ela não funcionaria por não incluir o ID.
 UPDATE sakila.staff
 SET first_name = 'Rannveig'
 WHERE first_name = 'Ravein';
 
--- Uma curiosidade sobre o UPDATE e o DELETE no MySQL Server é que, por padrão, existe uma configuração chamada safe updates mode que só vai te permitir executá-los caso eles incluam quais IDs devem ser modificados. Então, caso você tente fazer a query abaixo, ela não funcionaria por não incluir o ID.
--- UPDATE sakila.staff
--- SET first_name = 'Rannveig'
--- WHERE first_name = 'Ravein';
 -- Para evitar essa restrição, rode o seguinte comando em uma janela de query dentro do MySQL Workbench sempre que abri-lo para desabilitar essa funcionalidade, antes de executar seus comandos de UPDATE ou DELETE :
--- SET SQL_SAFE_UPDATES = 0;
+SET SQL_SAFE_UPDATES = 0;
 
 -- Alterando mais de uma coluna ao mesmo tempo
--- UPDATE sakila.staff
--- SET first_name = 'Rannveig', last_name = 'Jordan'
--- WHERE staff_id = 4;
--- UPDATE em massa
+UPDATE sakila.staff
+SET first_name = 'Rannveig', last_name = 'Jordan'
+WHERE staff_id = 4;
+
+
+--------UPDATE em massa--------
 -- Por questões de performance, para que apenas uma solicitação de query seja enviada ao servidor, podemos fazer uma atualização em massa.
--- -- Opção 1 - Incluindo a lista de condições fixas
--- UPDATE sakila.actor
--- SET first_name = 'JOE'
--- WHERE actor_id IN (1,2,3);
+-- Opção 1 - Incluindo a lista de condições fixas
+UPDATE sakila.actor
+SET first_name = 'JOE'
+WHERE actor_id IN (1,2,3);
 
 -- -- Opção 2 - Especificando como cada entrada será alterada individualmente
--- UPDATE sakila.actor
--- SET first_name = (
--- CASE actor_id WHEN 1 THEN 'JOE' -- se actor_id = 1, alterar first_name para 'JOE'
---               WHEN 2 THEN 'DAVIS' -- se actor_id = 2, alterar first_name para 'DAVIS'
---               WHEN 3 THEN 'CAROLINE' -- se actor_id = 3, alterar first_name para 'CAROLINE'
---           ELSE first_name -- em todos os outros casos, mantém-se o first_name
--- END);
+UPDATE sakila.actor
+SET first_name = (
+CASE actor_id WHEN 1 THEN 'JOE' -- se actor_id = 1, alterar first_name para 'JOE'
+              WHEN 2 THEN 'DAVIS' -- se actor_id = 2, alterar first_name para 'DAVIS'
+              WHEN 3 THEN 'CAROLINE' -- se actor_id = 3, alterar first_name para 'CAROLINE'
+          ELSE first_name -- em todos os outros casos, mantém-se o first_name
+END);
+
+-------------Fazendo um UPDATE de forma sequencial-------------------------
+-- Se o comando ORDER BY for usado juntamente com o UPDATE , os resultados serão alterados na ordem em que forem encontrados.
+-- Se o comando LIMIT for usado em conjunto com o UPDATE , um limite será imposto na quantidade de resultados que podem ser alterados. Caso contrário, todos os resultados que satisfizerem a condição serão atualizados.
+-- Veja a sintaxe abaixo. Lembre-se de que os valores entre colchetes ( [] ) são opcionais:
+UPDATE nome_da_tabela
+SET coluna1 = valor1, coluna2 = valor2
+[WHERE condições]
+[ORDER BY expressao [ ASC | DESC ]]
+[LIMIT quantidade_resultados];
+
+-- Exemplo:
+UPDATE sakila.staff
+SET password = 'FavorResetarSuaSenha123'
+WHERE active = 1
+ORDER BY last_update
+LIMIT 2;
+
+-- Essas são as maneiras mais comuns de utilizar o UPDATE no dia a dia.
+
+-- PAra saber sobreo modo --safe-updates consultar o curso bloco 20 parte 4 Pagian sobre UPDATE
+
+---------------Desafios UPDATE -----------------------------
+-- Como o banco pode ser deletado e recriado infinitamente, vamos desabilitar o --safe-updates nos exercícios. Além disso, esse modo pode ser habilitado novamente quando necessário. Rode o seguinte comando em uma janela de query dentro do MySQL Workbench sempre que abri-lo para desabilitar essa funcionalidade, antes de executar seus comandos UPDATE ou DELETE :
+SET SQL_SAFE_UPDATES = 0;
+
+-- Atualize o primeiro nome de todas as pessoas da tabela sakila.actor que possuem o primeiro nome "JULIA" para "JULES".
+
+
+-- Foi exigido que a categoria "Sci-Fi" seja alterada para "Science Fiction".
+
+
+-- Atualize o valor do aluguel para $25 de todos os filmes com duração maior que 100 minutos e que possuem a classificações "G" , "PG" ou "PG-13" e um custo de substituição maior que $20.
+
+
+-- Foi determinado pelo setor financeiro que haverá um reajuste em todos os preços dos filmes, com base em sua duração. Para todos os filmes com duração entre 0 e 100, o valor do aluguel passará a ser $10,00, e o aluguel dos filmes com duração acima de 100 passará a ser de $20,00.
+
+
+
 
