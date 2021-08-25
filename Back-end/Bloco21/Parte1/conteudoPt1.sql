b4230a65fffe8e25fdcbf48036f59bf9becc211a
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-08-25 16:35:12 -0300

    Iniciando dia 20.2.

--- Back-end/Bloco21/Parte1/conteudoPt1.sql
+++ Back-end/Bloco21/Parte1/conteudoPt1.sql
@@ -60,36 +60,38 @@ SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
 ---------------------------------------------------------------------------------------------
 ---------------- Condicionais ------------------------------------------------------
 -- Sintaxe:
-SELECT IF(condicao, valor_se_verdadeiro, valor_se_falso);
+-- SELECT IF(condicao, valor_se_verdadeiro, valor_se_falso);
+SET @idade = 10;c
+SELECT IF(@idade >= 18, 'Maior de idade', 'Menor de Idade');
 
-SELECT IF(idade >= 18, 'Maior de idade', 'Menor de Idade')
-FROM pessoas;
+SET @aberto = 0;
 
-SELECT IF(aberto, 'Entrada permitida', 'Entrada não permitida')
-FROM estabelecimentos;
+SELECT IF(@aberto, 'Entrada permitida', 'Entrada não permitida');
 
 -- Exemplo utilizando o banco sakila:
 SELECT first_name, IF(active, 'Cliente Ativo', 'Cliente Inativo') AS status
 FROM sakila.customer
 LIMIT 20;
 
+-----------------------------------------------------------------------------------------------
+--  Em situações em que é preciso comparar mais de uma condição, é preferível utilizar o CASE .
 
 -- Sintaxe:
-SELECT CASE
-  WHEN condicao THEN valor
-  ELSE valor padrao
-END;
+-- SELECT CASE
+--   WHEN condicao THEN valor
+--   ELSE valor padrao
+-- END;
 
-SELECT
-    nome,
-    nivel_acesso,
-    CASE
-        WHEN nivel_acesso = 1 THEN 'Nível de acesso 1'
-        WHEN nivel_acesso = 2 THEN 'Nível de acesso 2'
-        WHEN nivel_acesso = 3 THEN 'Nível de acesso 3'
-        ELSE 'Usuário sem acesso'
-    END AS nivel_acesso
-FROM permissoes_usuario;
+-- SELECT
+--     nome,
+--     nivel_acesso,
+--     CASE
+--         WHEN nivel_acesso = 1 THEN 'Nível de acesso 1'
+--         WHEN nivel_acesso = 2 THEN 'Nível de acesso 2'
+--         WHEN nivel_acesso = 3 THEN 'Nível de acesso 3'
+--         ELSE 'Usuário sem acesso'
+--     END AS nivel_acesso
+-- FROM permissoes_usuario;
 
 -- Em situações em que é preciso comparar mais de uma condição, é preferível utilizar o CASE .
 -- Exemplo utilizando a tabela sakila.film:
