ba9d7d2587bbf51ffe5b1f6b03dd450a084b0f7e
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-08-25 16:24:43 -0300

    Salvando progresso.

--- Back-end/Bloco21/Parte1/conteudoPt1.sql
+++ Back-end/Bloco21/Parte1/conteudoPt1.sql
@@ -37,22 +37,25 @@ SELECT SUBSTRING(title, 5, 2) FROM sakila.film WHERE film_id = 1;
 SELECT SUBSTRING(title, 5) FROM sakila.film WHERE film_id = 1;
 
 
-Para Fixar
+-- Para Fixar
 ----------------- Agora, vamos fixar os aprendizados com alguns desafios: ----------------------
 -- Faça uma query que exiba a palavra 'trybe' em CAIXA ALTA.
-
+SELECT UCASE('Trybe');
 
 -- Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?' .
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
 
 
 -- Utilizando uma query , encontre quantos caracteres temos em 'Uma frase qualquer' .
+SELECT LENGTH('Uma frase qualquer');
 
 
 -- Extraia e retorne a palavra "JavaScript" da frase 'A linguagem JavaScript está entre as mais usadas' .
 
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 13 , 11);
 
 -- Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa.
-
+SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
 
 ---------------------------------------------------------------------------------------------
 ---------------- Condicionais ------------------------------------------------------
