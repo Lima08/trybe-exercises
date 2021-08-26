809e89fde27daf2ebddde10f7c26e7630ff500a1
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-08-25 23:05:54 -0300

    Salvando progresso.

--- Back-end/Bloco21/Parte1/conteudoPt1.sql
+++ Back-end/Bloco21/Parte1/conteudoPt1.sql
@@ -110,7 +110,10 @@ LIMIT 10;
 
 ---------------------------- Para Fixar --------------------------------
 -- Usando o IF na tabela sakila.film , exiba o id do filme , o título e uma coluna extra chamada 'conheço o filme?' , em que deve-se avaliar se o nome do filme é ' ACE GOLDFINGER '. Caso seja, exiba "Já assisti a esse filme". Caso contrário, exiba "Não conheço o filme". Não esqueça de usar um alias para renomear a coluna da condicional.
-
+SELECT title FROM sakila.film
+WHERE title = 'ACE GOLDFING';
+SELECT film_id, title, IF(title = 'ACE GOLDFINGE', 'Já assisti a esse filme' , 'Não conheço o filme') AS `conheço o filme?` FROM sakila.film
+ORDER BY `conheço o filme?`;
 
 
 -- Usando o CASE na tabela sakila.film , exiba o título , a classificação indicativa ( rating ) e uma coluna extra que vamos chamar de 'público-alvo' , em que classificaremos o filme de acordo com as seguintes siglas:
@@ -119,11 +122,24 @@ LIMIT 10;
 -- PG-13: "Não recomendado para menores de 13 anos";
 -- R: "Não recomendado para menores de 17 anos";
 -- Se não cair em nenhuma das classificações anteriores: "Proibido para menores de idade".
+SELECT * FROM sakila.film;
+SELECT
+    title,
+    rating,
+    CASE
+        WHEN rating = 'G' THEN 'Livre para todos' 
+        WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
+        WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
+        WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
+        ELSE 'Proibido para menores de idade'
 
-
+    END AS `público-alvo`
+FROM sakila.film;
 
 -------------------------------------------------------------------------
 ----------------------------Funções matemáticas do MySQL-------------------------
+SELECT rental_duration  FROM sakila.film LIMIT 10;
+SELECT rental_rate  FROM sakila.film LIMIT 10;
 SELECT rental_duration + rental_rate FROM sakila.film LIMIT 10;
 SELECT rental_duration - rental_rate FROM sakila.film LIMIT 10;
 SELECT rental_duration / rental_rate FROM sakila.film LIMIT 10;
@@ -138,14 +154,15 @@ SELECT 13 DIV 2; -- 6
 -- Desafios com DIV e MOD-----------
 -- Dica: Números pares são aqueles que podem ser divididos em duas partes iguais. Ou seja, são aqueles cuja divisão por 2 retorna resto 0.
 -- Monte uma query usando o MOD juntamente com o IF para descobrir se o valor 15 é par ou ímpar. Chame essa coluna de 'Par ou Ímpar' , onde ela pode dizer 'Par' ou 'Ímpar'.
-
+SELECT IF(15 MOD(2) = 0, 'Par', 'Ímpar') AS ` Par ou Ímpar` ;
 
 
 -- Temos uma sala de cinema que comporta 220 pessoas. Quantos grupos completos de 12 pessoas podemos levar ao cinema sem que ninguém fique de fora?
-
+SELECT 220 DIV(12);
 
 
 -- Utilizando o resultado anterior, responda à seguinte pergunta: temos lugares sobrando? Se sim, quantos?
+SELECT 220 MOD(12);
 
 
 
@@ -171,7 +188,7 @@ SELECT FLOOR(10.2); -- 10
 SELECT POW(2, 2); -- 4
 SELECT POW(2, 4); -- 16
 SELECT SQRT(9); -- 3
-SELECT SQRT(16); -- 
+SELECT SQRT(16); -- 4
 
 ----------------------------------------------------------
 ---------------------Gerando valores aleatórios---------------
@@ -185,19 +202,19 @@ SELECT ROUND(7 + (RAND() * 6));
 
 ----------------- Para Fixar ----------------------------
 -- Monte uma query que gere um valor entre 15 e 20 .
-
+SELECT ROUND(15 + RAND() * 5);
 
 
 -- Monte uma query que exiba o valor arredondado de 15.7515971 com uma precisão de 5 casas decimais.
-
+SELECT ROUND(15.7515971 , 5);
 
 
 -- Estamos com uma média de 39.494 de vendas de camisas por mês. Qual é o valor aproximado para baixo dessa média?
-
+SELECT FLOOR(39.494);
 
 
 -- Temos uma taxa de inscrição de 85.234% no curso de fotografia para iniciantes. Qual é o valor aproximado para cima dessa média?
-
+SELECT CEIL(85.234);
 
 
 
@@ -230,15 +247,12 @@ SELECT HOUR(NOW()); -- retorna a hora atual
 
 -------------------- Para Fixar ------------------------------
 -- Para fixar, responda como seria possível encontrar as seguintes informações:
-
-
-
 -- Monte uma query que exiba a diferença de dias entre '2030-01-20' e hoje.
 
-
+SELECT DATEDIFF('2030-01-20', CURRENT_DATE());
 
 -- Monte uma query exiba a diferença de horas entre '10:25:45' e '11:00:00' .
-
+SELECT TIMEDIFF('10:25:45', '11:00:00');
 
 -------------------------------------------------------------------------------------------
 ------------------Utilizando as funções de agregação AVG , MIN , MAX , SUM e COUNT---------------------
@@ -253,28 +267,29 @@ SELECT COUNT(replacement_cost) FROM sakila.film; -- 1000 registros encontrados (
 -- Para praticar, vamos encontrar algumas informações sobre os filmes cadastrados em nossa base de dados.
 -- Monte um query que exiba:
 -- A média de duração dos filmes e dê o nome da coluna de 'Média de Duração';
-
-
+SELECT * FROM sakila.film;
+SELECT AVG(length) AS `Média de Duração` FROM sakila.film;
 
 -- A duração mínima dos filmes como 'Duração Mínima';
-
-
+SELECT MIN(length) AS `Duração minima` FROM sakila.film;
 
 -- A duração máxima dos filmes como 'Duração Máxima';
-
-
+SELECT MAX(length) AS `Duração Máxima` FROM sakila.film;
 
 -- A soma de todas as durações como 'Tempo de Exibição Total';
-
+SELECT SUM(length) AS `Tempo de Exibição Total` FROM sakila.film;
 
 
 -- E finalmente, a quantidade total de filmes cadastrados na tabela sakila.film como 'Filmes Registrados'.
+SELECT DISTINCT COUNT(title) AS `Filmes Registrados` FROM sakila.film;
 
 
 ------------------------------------------------------------------------------------------------------------
 ---------------------------Exibindo e filtrando dados de forma agrupada com GROUP BY e HAVING--------------
-SELECT coluna(s) FROM tabela
-GROUP BY coluna(s);
+-- SELECT coluna(s) FROM tabela
+-- GROUP BY coluna(s);
+
+SELECT first_name FROM sakila.actor;
 SELECT first_name FROM sakila.actor
 GROUP BY first_name;
 
@@ -306,18 +321,28 @@ GROUP by rating;
 
 ------------------------------- Praticando GROUP BY ---------------------------------------
 -- Monte uma query que exiba a quantidade de clientes cadastrados na tabela sakila.customer que estão ativos e a quantidade que estão inativos.
-
+SELECT * FROM sakila.customer;
+SELECT active, COUNT(active) FROM sakila.customer
+GROUP BY active;
 
 
 -- Monte uma query para a tabela sakila.customer que exiba a quantidade de clientes ativos e inativos por loja. Os resultados devem conter o ID da loja , o status dos clientes (ativos ou inativos) e a quantidade de clientes por status .
-
+SELECT * FROM sakila.customer;
+SELECT store_id, COUNT(active) FROM sakila.customer
+GROUP BY store_id, active;
 
 
 -- Monte uma query que exiba a média de duração de locação por classificação indicativa ( rating ) dos filmes cadastrados na tabela sakila.film . Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor.
-
+SELECT * FROM sakila.film;
+SELECT rating, AVG(length) FROM sakila.film
+GROUP BY rating
+ORDER BY  AVG(length) DESC;
 
 
 -- Monte uma query para a tabela sakila.address que exiba o nome do distrito e a quantidade de endereços registrados nele. Os resultados devem ser ordenados da maior quantidade para a menor.
+SELECT * FROM sakila.address;
+SELECT district, COUNT(address) FROM sakila.address
+GROUP BY district;
 
 
 ------------------------------------------------------------------------------------------
@@ -346,10 +371,11 @@ HAVING 'nomes cadastrados' > 2;
 
 --------------------------------------- Para Fixar---------------------------------------------------------
 -- Usando a query a seguir, exiba apenas as durações médias que estão entre 115.0 a 121.50. Além disso, dê um alias (apelido) à coluna gerada por AVG(length) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma decrescente.
-    SELECT rating, AVG(length)
+    SELECT rating, AVG(length) AS `Media duração`
     FROM sakila.film
-    GROUP BY rating;
-
+    WHERE length BETWEEN 115.0 AND 121.50
+    GROUP BY rating 
+    ORDER BY AVG(length) DESC;
 
 
 -- Usando a query a seguir, exiba apenas os valores de custo de substituição que estão acima de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma crescente.
