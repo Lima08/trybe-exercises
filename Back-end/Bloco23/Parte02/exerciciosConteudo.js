19687ff71734e4206d463d5416e2ff3ba4dc88a1
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-12 18:09:17 -0300

    finalizado ex parte 2 bloco 23.

--- Back-end/Bloco23/Parte02/exerciciosConteudo.js
+++ Back-end/Bloco23/Parte02/exerciciosConteudo.js
@@ -81,15 +81,35 @@ db.restaurants.find({rating: {$lt: 4}}).count();
 db.restaurants.find({rating: {$nin:[5,6,7 ]}}).count();
 
 // Selecione e faça a contagem dos restaurantes que não possuem avaliação menor ou igual a 5 , essa consulta também deve retornar restaurantes que não possuem o campo avaliação.
-
+db.restaurants.find({ $or: [ {rating: {$gt:5}}, {rating: {$exists: false}}  ]}).count();
 
 // Selecione e faça a contagem dos restaurantes em que a avaliação seja maior ou igual a 6 , ou restaurantes localizados no bairro Brooklyn .
-
+db.restaurants.find({ $or: [ {rating: {$gte:6}}, {borough: "Brooklyn"} ]}).count();
 
 // Selecione e faça a contagem dos restaurantes localizados nos bairros Queens , Staten Island e Broklyn e possuem avaliação maior que 4 .
+db.restaurants.find({ $and: [ {borough: {$in: ["Brooklyn", "Staten Island", "Queens"]}}, {rating: {$gt: 4}} ]}).count();
 
 
 // Selecione e faça a contagem dos restaurantes onde nem o campo avaliação seja igual a 1 , nem o campo culinária seja do tipo American .
-
+db.restaurants.find({ $and: [ {rating: {$not:/1/}}, {cuisine: {$not:/American/}} ]}).count();
 
 // Selecione e faça a contagem dos resturantes em que a avaliação seja maior que 6 ou menor que 10 , E esteja localizado no bairro Brooklyn , OU não possuem culinária do tipo Delicatessen .
+db.restaurants.find({ $and:[ {rating: {$gt:6, $lt: 10}}, {$or:[ {borough:"Brooklyn"}, {cuisine: {$not: /Delicatessen/}}]}]}).count();
+
+//  ---- EX Fixação pt 2 --------------------------------
+// Faça os desafios 1 e 2 abaixo sobre o sort() utilizando a coleção restaurants criada anteriormente.
+
+// Ordene alfabeticamente os restaurantes pelo nome (atributo name ).
+db.restaurants.find().sort({name: 1});
+
+
+// Ordene os restaurantes de forma descrescente baseado nas avaliações.
+db.restaurants.find().sort({rating:-1})
+
+//  Ex: REmovendo documentos -------------
+// Faça os desafios 1 e 2 abaixo, sobre remoção de documentos utilizando a coleção restaurants criada anteriormente.
+// Remova o primeiro restaurante que possua culinária do tipo Ice Cream, Gelato, Yogurt, Ices .
+db.restaurants.deleteOne({cuirine: {$in: ["Ice Cream", "Gelato" , "Yogurt" , "Ices"]}});
+
+// Remova todos os restaurantes que possuem culinária do tipo American .
+db.restaurants.deleteMany({cuisine: "American"});
